2b37978fcf0267773670f738e5ccfd5505a5e26e
lchaconc <luis.chacon.campos@gmail.com>
2024-10-04 11:50:58 -0600

    imp de capa marco para menu personakes

--- src/scenes/MenuPersonajes.js
+++ src/scenes/MenuPersonajes.js
@@ -3,59 +3,84 @@ import { Scene } from "phaser";
 export default class MenuPersonajes extends Scene {
     constructor() {
         super("MenuPersonajes");
-        this.personajesSprites = []; // Array para almacenar los sprites de los personajes
     }
 
-    init () {
-        this.center = this.registry.get("center");
-        this.personajes = this.registry.get("personajes");
-        this.indice = 0; // Índice inicial
+    init() {
+        this.center = this.registry.get("center");  // Coordenadas del centro desde Boot
+        this.personajes = this.registry.get("personajes");  // Lista de personajes
+        this.indice = 0;  // Índice del personaje actual
     }
 
-    create () {
+    create() {
+        // Fondo
         this.add.image(this.center.x, this.center.y, "bgPersonajes").setOrigin(0.5);
+        
+        
+
+        
+
+        // Renderizar los personajes iniciales
+        this.renderPersonajes(this.indice);
+
+        // Flechas de navegación
         const btnFlechaIzquierdaVerde = this.add.image(this.center.x - 290, this.center.y, "btnFlechaIzquierdaVerde").setOrigin(0.5).setInteractive();
         const btnFlechaDerechaVerde = this.add.image(this.center.x + 290, this.center.y, "btnFlechaDerechaVerde").setOrigin(0.5).setInteractive();
 
         btnFlechaIzquierdaVerde.on("pointerdown", () => this.handleAvanzar("i"));
         btnFlechaDerechaVerde.on("pointerdown", () => this.handleAvanzar("d"));
 
-        this.renderPersonajes(this.indice); // Renderiza la primera vez los personajes
+        this.add.image(this.center.x , this.center.y, "bgPersonajes2" ).setOrigin(0.5);
+
+        
     }
 
     update () {
-        // Aquí no es necesario hacer nada ya que la actualización la manejamos en handleAvanzar
+        this.add.image(this.center.x , this.center.y, "bgPersonajes2" ).setOrigin(0.5);
     }
 
     renderPersonajes(i) {
-        // Limpiar los personajes anteriores
-        this.personajesSprites.forEach(sprite => sprite.destroy());
-        this.personajesSprites = []; // Vaciar el array de personajes
+        // Destruir personajes anteriores si existen
+        if (this.personajeCentral) {
+            this.personajeCentral.destroy();
+        }
+        if (this.personajeIzquierda) {
+            this.personajeIzquierda.destroy();
+        }
+        if (this.personajeDerecha) {
+            this.personajeDerecha.destroy();
+        }
 
-        // Obtener los índices de los personajes en el carrusel
-        const prevIndex = (i - 1 + this.personajes.length) % this.personajes.length;
-        const currIndex = i;
-        const nextIndex = (i + 1) % this.personajes.length;
-
-        // Renderizar los personajes con los índices calculados
-        const personaje1 = this.add.image(this.center.x - 120, this.center.y, this.personajes[prevIndex].id).setScale(0.4);
-        const personaje2 = this.add.image(this.center.x, this.center.y, this.personajes[currIndex].id).setScale(0.9);
-        const personaje3 = this.add.image(this.center.x + 120, this.center.y, this.personajes[nextIndex].id).setScale(0.4);
-
-        // Guardar los sprites para luego eliminarlos
-        this.personajesSprites.push(personaje1, personaje2, personaje3);
+        // Renderizar nuevos personajes
+        this.personajeIzquierda = this.add.image(this.center.x - 120, this.center.y, this.personajes[i - 1 < 0 ? this.personajes.length - 1 : i - 1].id).setScale(0.4);
+        this.personajeCentral = this.add.image(this.center.x, this.center.y, this.personajes[i].id).setScale(0.9);
+        this.personajeDerecha = this.add.image(this.center.x + 120, this.center.y, this.personajes[(i + 1) % this.personajes.length].id).setScale(0.4);
     }
 
     handleAvanzar(direccion) {
-        // Asegurar que el índice se mantenga en el rango correcto (carrusel)
         if (direccion === "d") {
             this.indice = (this.indice + 1) % this.personajes.length;
         } else if (direccion === "i") {
             this.indice = (this.indice - 1 + this.personajes.length) % this.personajes.length;
         }
 
-        // Vuelve a renderizar los personajes con el nuevo índice
-        this.renderPersonajes(this.indice);
-        console.log("Índice actual: ", this.indice);
+        // Animar el personaje central saliendo
+        this.tweens.add({
+            targets: this.personajeCentral,
+            x: this.center.x + (direccion === "d" ? -120 : 120),
+            scaleX: 0.4,
+            scaleY: 0.4,
+            duration: 500,
+            onComplete: () => {
+                // Cambiar el personaje central después de la animación de salida
+                this.renderPersonajes(this.indice);
+            }
+        });
+
+        // Mientras el central sale, animar los otros dos
+        this.tweens.add({
+            targets: [this.personajeIzquierda, this.personajeDerecha],
+            x: (target) => target.x + (direccion === "d" ? -120 : 120),
+            duration: 500,
+        });
     }
 }
